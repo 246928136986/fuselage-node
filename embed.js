f317b888735b73db6b0f11314e8eba0c3d8abec7
246928136986 <math.trigonometry.alt.291@gmail.com>
2025-03-15 12:44:42 -0500

    Update embed.js

--- embed.js
+++ embed.js
@@ -10,10 +10,24 @@ try {
 
 registerSW()
   .then(() => {
-    window.open(
-      __uv$config.prefix + __uv$config.encodeUrl(destination),
-      "_self"
-    );
+
+                const win = window.open();
+                if (!win) {
+                    alert("Pop-up blocked. Please allow pop-ups for this site.");
+                }
+        
+                win.document.body.style.margin = "0";
+                win.document.body.style.height = "100vh";
+                const frm = win.document.createElement("iframe");
+                frm.style.border = "none";
+                frm.style.width = "100%";
+                frm.style.height = "100%";
+                frm.style.margin = "0";
+                frm.referrerpolicy = "no-referrer";
+                frm.allow = "fullscreen";
+                frm.src = __uv$config.prefix + __uv$config.encodeUrl(destination);
+                win.document.body.appendChild(frm);
+
   })
   .catch((err) => {
     alert(`Your boat crashed!\nAn error occured (you probably need to stop using http you moron)\n${err}`);
